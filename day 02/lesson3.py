b48321f780c9a7eb2e054c96b8f71b5dbae68c37
Nodar shavadze <nodo.boom1@gmail.com>
2024-09-15 13:59:31 +0400

    second commit

---
+++ day 02/lesson3.py
@@ -0,0 +1,7 @@
+new_friend = "data"
+mycook = "i know making fried egg and some drinks like cocoa and tea"
+why_happy = "i joined a goal oriented academy. it is best academy ever. and now im learning to how to write codes in python. and also teachers are so good."
+also_friend = "i also get new friend named"
+print(why_happy + " " + also_friend + " " + new_friend + " " + mycook)
+
+
