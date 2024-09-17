01cd7dce85e420b3ff3b2c6bf133e33f8f1793e7
Nodar shavadze <nodo.boom1@gmail.com>
2024-09-17 20:22:25 +0400

    second commit

---
+++ day 03/lesson5.py
@@ -0,0 +1,10 @@
+name = "nodar"
+surname = "shavadze"
+print(12160-340+1506)
+print(10495+49294*2)
+print(240-120+180-249-10+4504)
+print(1948-194+14949)
+print(1513+14135-240)
+#ცვლადის შესაცმნელად საჭიროა მოვიფიქროდ ცვლადის სახელი და ცვლადის მნშვნელობა მაგალითად name = nodo ამ შემთხვევაში name არის ცვლადი და nodo ცვლადის მნიშვნელობა, = კი საჭიროა იმისთვის რომცვლადს მიანიჭოს მნიშვნელობა
+print("i like goa because goa have very kind and cool mentors, kids are able to learn coding perfectly, also mentors have a great patience, also group leader and mentor will help you if you have any pronlem in proggramimg, there is also physical activities that help us to save our health.")
+print(name + " " + surname)
