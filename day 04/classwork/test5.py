3673c91f4364cb55828d9e5f19f18d0dd44d534c
Nodar shavadze <nodo.boom1@gmail.com>
2024-09-22 21:47:08 +0400

    second commit

---
+++ day 04/classwork/test5.py
@@ -0,0 +1,4 @@
+name = input("please put your name: ")
+surname = input("put your surname: ")
+age = input("put your age: ")
+print("my name is" + " " + name + " " + "my surname is" + " " + surname + " " + "im" + " " + age)
---
+++ day 04/homework/ex5.py
@@ -0,0 +1,32 @@
+print(545+47247)
+print(174+1374)
+print(57+34)
+print(13513+1353)
+print(567+918)
+print(34-13)
+print(1413-1235)
+print(13513-11244)
+print(4626-2535)
+print(25456-2235)
+print(1123*1244)
+print(9495*3251)
+print(26523*2355)
+print(154*1353)
+print(1343*311)
+print(25232/1340)
+print(2542/235)
+print(32525/2532)
+print(253/235)
+print(11351/141)
+age = 11
+age = 16
+print(age)
+age2 = 8
+age3 = 9
+age4 = 42
+age5 = 43
+age6 = 66
+print(age + age3 + age4 + age5 + age6) 
+name = "nodo"
+name = "Data"
+print(name)
