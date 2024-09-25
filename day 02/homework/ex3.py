fd8e94e0051c7a69ef08f3aaa143d19fe9dc88ea
Nodar shavadze <nodo.boom1@gmail.com>
2024-09-25 15:31:54 +0400

    second commit

---
+++ day 02/homework/ex3.py
@@ -0,0 +1,6 @@
+new_friend = "gio"
+mycook = "i know making fried egg and some drinks like cocoa and tea"
+why_happy = "i joined a goal oriented academy. it is best academy ever. and now im learning to how to write codes in python. and also teachers are so good."
+also_friend = "i meet a student named"
+print(why_happy + " " + also_friend + " " + new_friend + " " + mycook)
+
