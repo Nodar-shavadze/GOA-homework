2ac09f32ba7779331c18d3430b5b19a2e410ede2
Nodar shavadze <nodo.boom1@gmail.com>
2024-10-01 13:23:12 +0400

    second commit

---
+++ day 07/homework/ex8.py
@@ -0,0 +1,15 @@
+str = "hello"
+int = 132
+float = 13.45
+know_reading = True
+print(type(str))
+print(type(int))
+print(type(float))
+print(type(know_reading))
+
+name = input("write your name: ")
+age = input("type your age: ")
+height = input("type your height")
+favcolor = input("type your favorite color: ")
+
+print("hello im" + " " + name + " " + "im" + " " + age + " " + "years old. im" + " " + height + " " + "cm. my favorite color is" + " " + favcolor)
