ef2ad378b7469d9c1c308e01653ddd7d64c269d9
Nodar shavadze <nodo.boom1@gmail.com>
2024-10-09 12:17:09 +0400

    second commit

---
+++ day 09/classwork/test10.py
@@ -0,0 +1,14 @@
+age = input("type ur age: ")
+print(int(age) // 5)
+#დავალება 1
+
+name = input("write ur name: ")
+num = 23
+print(name * num)
+
+#დავალება 2
+
+age2 = input("type ur age: ")
+print(type(age2))
+#დავალება 3
+
---
+++ day 09/homework/ex11.py
@@ -0,0 +1,22 @@
+#დავალება 1
+age = int(input("put your age here: "))
+print(age // 10)
+
+#დავალება 2
+name = input("type ur name: ")
+print("nodo" >= name)
+
+#დავალება 3
+num1 = int(input("just type any number here: "))
+num2 = int(input("just type there any number again: "))
+print(num1 % num2)
+
+#დავალება 4
+age2 = int(input("print your age again please: "))
+print(age2 % 2)
+
+
+
+
+
+
